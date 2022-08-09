77d17d54cf5d65abfd866b2bffd37816dca9193d
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-08-09 17:41:35 +0100

    write in another version

--- model.py
+++ model.py
@@ -16,16 +16,30 @@ class CNNModel(nn.Module):
 		## define the model architecture here
 		## MNIST image input size batch * 28 * 28 (one input channel)
 		##-----------------------------------------------------------
-		padding = (args.k_size-1)/2
-		## define CNN layers below
-		self.conv1 = nn.Conv2d(in_channels=1,out_channels=args.channel_out1,kernel_size= args.k_size, stride= args.stride,padding= padding)
-
-		self.conv2 = nn.Conv2d(in_channels= args.channel_out1, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride, padding= padding)
-
-		# self.conv3 = nn.Conv2d(in_channels= args.channel_out2, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride, padding= padding),
-		self.maxPool = nn.MaxPool2d(kernel_size=args.pooling_size,stride= args.stride)
-		self.activation = args.activation
-		self.dropout = args.dropout
+		# padding = (args.k_size-1)/2
+		# ## define CNN layers below
+		# self.conv1 = nn.Conv2d(in_channels=1,out_channels=args.channel_out1,kernel_size= args.k_size, stride= args.stride,padding= padding)
+		#
+		# self.conv2 = nn.Conv2d(in_channels= args.channel_out1, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride, padding= padding)
+		#
+		# # self.conv3 = nn.Conv2d(in_channels= args.channel_out2, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride, padding= padding),
+		# self.maxPool = nn.MaxPool2d(kernel_size=args.pooling_size,stride= args.stride)
+		# self.activation = args.activation
+		# self.dropout = args.dropout
+		self.cov = nn.Sequential(
+			nn.Conv2d(in_channels=1,out_channels=args.channel_out1,kernel_size= args.k_size, stride= args.stride),
+			nn.BatchNorm2d(args.channel_out1),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=args.pooling_size,stride= args.stride),
+			nn.Conv2d(in_channels= args.channel_out1, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride),
+			nn.BatchNorm2d(args.channel_out2),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=args.pooling_size,stride= args.stride),
+			nn.Conv2d(in_channels= args.channel_out1, out_channels=args.channel_out2,kernel_size=args.k_size, stride = args.stride),
+			nn.BatchNorm2d(args.channel_out2),
+			nn.ReLU(),
+			nn.MaxPool2d(kernel_size=args.pooling_size,stride= args.stride)
+		)
 
 		##------------------------------------------------
 		## write code to define fully connected layer below
@@ -41,20 +55,21 @@ class CNNModel(nn.Module):
 		##---------------------------------------------------------
 		## write code to feed input features to the CNN models defined above
 		##---------------------------------------------------------
-		print("shape of image",x.unsqueeze(dim=0).shape)
-		x = self.conv1(x)
-		x = self.activation(x)
-		x = self.maxPool(x)
-		x = self.conv2(x)
-		x = self.activation(x)
-		x = self.maxPool(x)
-		x = self.conv2(x)
-		x = self.activation(x)
-		x = self.maxPool(x)
-		x= self.dropout(x)
+		# print("shape of image",x.unsqueeze(dim=0).shape)
+		# x = self.conv1(x)
+		# x = self.activation(x)
+		# x = self.maxPool(x)
+		# x = self.conv2(x)
+		# x = self.activation(x)
+		# x = self.maxPool(x)
+		# x = self.conv2(x)
+		# x = self.activation(x)
+		# x = self.maxPool(x)
+		# x= self.dropout(x)
+		x_out = self.cov(x)
 
 		## write flatten tensor code below (it is done)
-		# x = torch.flatten(x_out,1) # x_out is output of last layer
+		x = torch.flatten(x_out,1) # x_out is output of last layer
 
 		## ---------------------------------------------------
 		## write fully connected layer (Linear layer) below
