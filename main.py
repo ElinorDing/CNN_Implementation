c0db7b45d6831b04676b901077c34ddf286ff036
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2022-08-12 13:35:25 +0100

    save fig

--- main.py
+++ main.py
@@ -251,7 +251,7 @@ def main():
 		plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
 		plt.xlabel('number of training examples seen')
 		plt.ylabel('negative log likelihood loss')
-		plt.show()
+		plt.savefig('Train-Loss.svg')
 
 	for i in range(6):
 		plt.subplot(2,3,i+1)
@@ -262,7 +262,7 @@ def main():
 		# 	y_pred.data.max(1, keepdim=True)[1][i].item()))
 		plt.xticks([])
 		plt.yticks([])
-		plt.show()
+		plt.savefig('Prediction.svg')
 
 
 
